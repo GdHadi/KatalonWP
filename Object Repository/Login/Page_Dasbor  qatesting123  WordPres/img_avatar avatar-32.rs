c6bdb84d013e036236e41b3357a28b753ea8428f
GdHadi <igede.nurhadi@gmail.com>
2018-05-12 13:54:29 +0800

    TC successfully login

---
+++ Object Repository/Login/Page_Dasbor  qatesting123  WordPres/img_avatar avatar-32.rs
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_avatar avatar-32</name>
+   <tag></tag>
+   <elementGuidId>a3be6660-5f4c-4373-b0e9-455aa91849d6</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>src</name>
+      <type>Main</type>
+      <value>https://1.gravatar.com/avatar/a850195e4abe9cd4be632426d128a972?s=32&amp;d=mm&amp;r=G</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>avatar avatar-32</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>height</name>
+      <type>Main</type>
+      <value>32</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>width</name>
+      <type>Main</type>
+      <value>32</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;wp-admin-bar-my-account&quot;)/a[@class=&quot;ab-item&quot;]/img[@class=&quot;avatar avatar-32&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
