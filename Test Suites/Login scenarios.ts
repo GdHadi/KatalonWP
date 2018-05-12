ea69097c26858ae6234e6a4dab128faea32a77ec
GdHadi <igede.nurhadi@gmail.com>
2018-05-12 14:14:11 +0800

    Add test suite

---
+++ Test Suites/Login scenarios.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login scenarios</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-12T14:13:13</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4b1597df-7438-475d-899c-42f0c4c08e46</testSuiteGuid>
+   <testCaseLink>
+      <guid>d8abcf2f-7aaf-4ec2-9fe9-9a51bafc848f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Successfully Login</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>27a94d13-ebb2-4b6a-93c8-9c3d9065b5f7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6eeb6821-ceee-42a9-b1d0-194696099781</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>594df381-dc89-4c5d-bf1a-6e8827aed703</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
