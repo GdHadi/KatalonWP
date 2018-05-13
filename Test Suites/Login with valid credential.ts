bec75fbc42d68c300c25a425090efcf96c653538
GdHadi <igede.nurhadi@gmail.com>
2018-05-13 13:24:21 +0800

    Run test from CLI

---
+++ Test Suites/Login with valid credential.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login with valid credential</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-13T13:15:05</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f066ac86-e0c1-473d-87e0-5e20d8374d1f</testSuiteGuid>
+   <testCaseLink>
+      <guid>fcefa6fd-7e25-4284-9231-767cd2fc6449</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login with valid a credential</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
